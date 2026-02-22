56afcd72ba7852478707de97789c3b1b3a7367d7
ZHAO Xudong <zxdong@gmail.com>
2026-02-22 10:31:08 +0800

    Fix tests

--- tests/unit/utils.test.js
+++ tests/unit/utils.test.js
@@ -5,7 +5,6 @@ import {
   DUMP_DRAWS, 
   DUMP_CANVASES, 
   EMPTY_GIF_IMAGE, 
-  combine_array_buffers, 
   arraybuffer_to_str, 
   keycode_to_start_scan, 
   keycode_to_end_scan 
@@ -22,27 +21,6 @@ describe('Utils', () => {
 
   test('should export correct empty gif image', () => {
     expect(EMPTY_GIF_IMAGE).toBe("data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=");
-  });
-
-  test('should combine array buffers correctly', () => {
-    const buffer1 = new ArrayBuffer(2);
-    const view1 = new Uint8Array(buffer1);
-    view1[0] = 1;
-    view1[1] = 2;
-
-    const buffer2 = new ArrayBuffer(2);
-    const view2 = new Uint8Array(buffer2);
-    view2[0] = 3;
-    view2[1] = 4;
-
-    const combined = combine_array_buffers(buffer1, buffer2);
-    const combinedView = new Uint8Array(combined);
-
-    expect(combined.byteLength).toBe(4);
-    expect(combinedView[0]).toBe(1);
-    expect(combinedView[1]).toBe(2);
-    expect(combinedView[2]).toBe(3);
-    expect(combinedView[3]).toBe(4);
   });
 
   test('should convert array buffer to string correctly', () => {
